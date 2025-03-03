ab1b9175ea1b5916386ad4a701104f92880bfa3e
Alexandra Onose <alexandraonose05@yahoo.com>
2025-03-03 16:28:31 +0200

    Add list and button operations

--- Calculator/Calculator/Models/CalculatorModel.cs
+++ Calculator/Calculator/Models/CalculatorModel.cs
@@ -12,6 +12,10 @@ namespace Calculator.Models
 {
     class CalculatorModel
     {
+        private static List<double> memoryStack = new List<double>();
+
+
+
         #region Math Operations
         static public double Add(double a, double b)
         {
@@ -64,8 +68,92 @@ namespace Calculator.Models
         }
         #endregion
 
+        #region Button Operations
+        static public void Backspace(ref string text)
+        {
+            if (text.Length > 0)
+            {
+                text = text.Remove(text.Length - 1);
+            }
+        }
+        public static void CE(ref string text)
+        {
+            text = "0"; // Resetează ultima valoare introdusă
+        }
+
+        public static void C(ref string text)
+        {
+            text = "";
+        }
+
+        public static void MC()
+        {
+            memoryStack.Clear(); // Șterge întreaga memorie
+        }
+
+        public static void MPlus(ref string text)
+        {
+            if (double.TryParse(text, out double value))
+            {
+                if (memoryStack.Any())
+                    memoryStack[memoryStack.Count - 1] += value;
+                else
+                    memoryStack.Add(value);
+            }
+        }
+
+        public static void MMinus(ref string text)
+        {
+            if (double.TryParse(text, out double value))
+            {
+                if (memoryStack.Any())
+                    memoryStack[memoryStack.Count - 1] -= value;
+                else
+                    memoryStack.Add(-value);
+            }
+        }
+
+        public static void MS(ref string text)
+        {
+            if (double.TryParse(text, out double value))
+            {
+                memoryStack.Add(value);
+            }
+        }
+
+        public static void MR(ref string text)
+        {
+            if (memoryStack.Any())
+            {
+                text = memoryStack.Last().ToString();
+            }
+        }
+
+        public static void MGreater(ref string text)
+        {
+            if (memoryStack.Any())
+            {
+                text = string.Join(", ", memoryStack);
+            }
+        }
+        #endregion
 
 
+        #region Digital Grouping
+        static public void AddDigit(ref string text, string digit)
+        {
+            text += digit;
+        }
+        static public void AddDot(ref string text)
+        {
+            if (!text.Contains("."))
+            {
+                text += ".";
+            }
+        }
+
+
+        #endregion
 
 
 
